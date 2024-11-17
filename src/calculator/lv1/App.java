bedd993cb0684a4c60cb4968be35c516cca91717
joacho <joacho1205@gmail.com>
2024-11-17 17:40:19 +0900

    1117

--- src/calculator/lv1/App.java
+++ src/calculator/lv1/App.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in)
 
         while (true) {
             System.out.print("첫 번째 숫자를 입력하세요: ");
